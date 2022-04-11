730871960c47aef142e3afb51e959db0ed4c1495
songh_home <songh-cc@foxmail.com>
2022-04-11 23:09:24 +0800

    完善了说明文档，新增日志的记录方式

--- 03服务器WSGI/dynamic_sources/dynamic_decorator.py
+++ 03服务器WSGI/dynamic_sources/dynamic_decorator.py
@@ -16,9 +16,9 @@ def conncet_db():
             host="192.168.125.135",
             port=3306,
             user="root",
-            passwd="123456",  # 除了port，密码这些一定要str
+            passwd="123456",  # (1):除了port，密码这些一定要str
             charset="utf8",
-            database="my_areas"
+            database="my_areas"  # (2):注意这是表示用的哪一个库
         )
     return dbconnect
 
@@ -26,7 +26,8 @@ def conncet_db():
 "这个一导包进来，带@route的行就会执行"
 def route(url):
     def set_func(func):
-        URL_FUNC_DICT[url] = func  # 这里在@route时就会执行，装饰器带参数讲原理时讲过
+        # (3):这里在@route时就会执行，装饰器带参数讲原理时讲过
+        URL_FUNC_DICT[url] = func
 
         def call_func(*args, **kwargs):  # 因为一开始字典里就拿到了 func的引用，其实这下面三行可以不要的，
             return func(*args, **kwargs)  # 因为不会单独再去使用类似 register() 的单独调用，没有这三行，这样的单独调用就会报错
@@ -64,11 +65,7 @@ def show():
         return "<div>Error!!! 数据库连接失败，请检查密码是否正确，或数据库是否开启！</div>"
 
     cursor = dbconnect.cursor()
-    # 这里的 "四川省" 可以换成其它的，也可以换成市,比如 “广安”
-    # query_sql = "select * from areas as province inner join areas as city on province.district_id=city.pid " \
-    #             "having province.district_id=(select district_id from areas where district='成都');"
-    # 修改了一下查询的表，然后把下面的表结构也做了删减
-    query_sql = "select * from province;"
+    query_sql = "select * from province;"  # 表示用的 province 这个表
 
     cursor.execute(query_sql)
     datas = cursor.fetchall()
@@ -87,8 +84,8 @@ def show():
             <th>添加自选</th>
         </tr>
     """
-    # 这每行的内容 (注意这里的 <a href='show/%s.py'> ，这里有个 %s 代表每个省份具体的数字)
-    line = """
+    # (4):注意这里的 <a href='show/%s.py'> ，这里有个 %s 代表每个省份具体的数字   # 这每行的内容
+    line = """  
         <tr>
             <td>%s</td>
             <td>%s</td>
@@ -163,10 +160,11 @@ def collect():
     return contents
 
 
-# 注意点.前面的反斜杠，一定要，后面正则的时候影响很大
+# (5):这种路由里，写的是一个正则表达式，就对应 /show/12.py  /show/15.py 这种只是后面参数不一样(点的按钮不一样)，处理逻辑都是一样的
+# (6):注意点.前面的反斜杠，一定要，不然下面application函数中正则的影响很大
 @route("/show/(\d+)\.py")  # 从展示的主页面收藏数据
 def collect_add(ret):
-    "ret是传进来的正则匹配结果(且一定是匹配到的，没配到的进不来).group(1)就是对应的各省份的district_id "
+    "ret是传进来的正则匹配结果(且一定是匹配到的，没配到的进不来)，ret.group(1)就是对应的各省份的district_id "
     district_id = ret.group(1)
 
     try:  # 要检查连接容错
@@ -176,14 +174,15 @@ def collect_add(ret):
     cursor = dbconnect.cursor()
 
     query_sql = "select * from province where district_id=%s;"
-    cursor.execute(query_sql, (district_id, ))  # 注意写元祖进去，不要自己在上面.format()拼接，小心sql注入
+    # (7):注意写元祖进去，不要自己在上面.format()拼接，小心sql注入
+    cursor.execute(query_sql, (district_id, ))
     query_data = cursor.fetchall()
 
-    # (1)判断这个收藏的id是否在数据库的范围内，若不在就没有数据，就是认为模拟的数据：
+    # 首先：判断这个收藏的id是否在数据库的范围内，若不在就没有数据，就是认为模拟的数据：
     if not query_data:
         results = "行行好，大哥手下留情，不要再攻击我们了..."
     else:
-        # (2)如果数据合法，还要判断是否已经收藏了
+        # 其次：如果数据合法，还要判断是否已经收藏了
         # query_collect = "select district_id, district, level from collection where district_id=%s"
         query_collect = "select * from collection where district_id=%s"   # 这是个人收藏表
         cursor.execute(query_collect, (district_id,))
@@ -194,12 +193,15 @@ def collect_add(ret):
             cursor.execute("select district_id, district, level from province where district_id=%s", (district_id, ))
             insert_data = cursor.fetchone()
 
-            # 注意这里的第二个%s，外面加了一对''
-            # 不加''就是 insert into collection values(default, 22, 山东省, 1);  # 山东省是字符串，这句是执行不了的，
+            """
+                注意这里这种自己拼接"insert into collection values(default, %s, '%s', %s);"的第二个%s，外面加了一对''，
+            不加''就是 insert into collection values(default, 22, 山东省, 1);  # 山东省是字符串，这句是执行不了的，
+            所以不要自己去拼接，像下面去自己执行
+            """
             insert_sql = "insert into collection values(default, %s, %s, %s);"
-            # cursor.execute(insert_sql, (insert_data[0], insert_data[1], insert_data[2]))
+            # cursor.execute(insert_sql, (insert_data[0], insert_data[1], insert_data[2]))  # 和下面是一样的
             cursor.execute(insert_sql, insert_data)
-            dbconnect.commit()   # 一定是链接来commit
+            dbconnect.commit()   # (6):一定是链接来commit
             results = "收藏成功！"
 
     cursor.close()
@@ -233,7 +235,7 @@ def collect_del(ret):
     cursor.close()
     dbconnect.close()
 
-    # 注意：这里返回收藏页面，是以 :8899/collect.py 作为相对路径的，
+    # (8):注意：这里返回收藏页面，是以 :8899/collect.py 作为相对路径的，
     # - href=".." 就回到了 :8899/
     # - href="../collect.py"  就来到了 :8899/collect.py
     results += "<div><a href='../collect.py'><strong style='width:70px; height:25px'>返回收藏页面</strong></a></div>"
@@ -250,8 +252,8 @@ def application(env: dict, start_response):
     if file_name.endswith(".py"):
 
         try:
-            # 这是为了匹配,是@route("/show/(\d+)\.py")这个格式再把正则匹配的结果传家进去，否则是没有参数的
-            # 这种一般传进来的是 /show/123.py
+            # (9)这是为了匹配,是@route("/show/(\d+)\.py")这个格式再把正则匹配的结果传家进去，否则是没有参数的
+            # 这种一般传进来的是 /show/13.py  # 13其实就是某个省份的district_id
             match_out = re.match("/show/(\d+)\.py", file_name)
             if match_out:
                 ret = URL_FUNC_DICT["/show/(\d+)\.py"]  # 这里返回的就是对应的函数引用(因为上限@路由关于这个的名字写死了的)
@@ -269,7 +271,7 @@ def application(env: dict, start_response):
             return "这个页面找不到了！ 404 Not Found！"
     else:  # 否则就是访问静态资源
         try:
-            # 一定注意，这里./static_sources这个相对路径是相对的server.py(因为是从server.py启动的)，而不是dynamic.py
+            # (10):一定注意，这里./static_sources这个相对路径是相对的server.py(因为是从server.py启动的)，而不是dynamic.py
             fp = open(r"./static_sources" + file_name, "r")
         except Exception:
             response_body = "这个页面找不到了！ 404 Not Found！"
---
+++ 日志模块.py
@@ -0,0 +1,57 @@
+import logging
+
+# 通过传参决定日志等级
+# import sys
+# print(sys.argv)
+# level = sys.argv[1]
+#
+# numeric_level = getattr(logging, level)
+# logging.basicConfig(level=numeric_level)
+
+"(1)、安静日志输出到控制台"
+# # 这个等级就代表了 logging.INFO即其以上的才会被记录
+# # 若不给这个，默认等级为 logging.WARNING
+# format格式不是必须的，但是这样会更加直观，定位了时间，错误位置
+# logging.basicConfig(level=logging.INFO,
+#                     format="%(asctime)s - %(filename)s[line:%(lineno)d] - %(levelname)s: %(message)s")
+
+
+"(2)、将日志输出到文件"
+# # win上用的gbk，好像无法指定encoding
+# logging.basicConfig(level=logging.WARNING,
+#                     filename="./log.txt",
+#                     filemode="w",       # 这个跟open的模式是一样的,it defaults to 'a'
+#                     format="%(asctime)s - %(filename)s[line:%(lineno)d] - %(levelname)s: %(message)s")
+
+
+# "(3)、既要把日志输出到控制台，也要写入到日志文件"
+# 第一步：创建一个logger
+logger = logging.getLogger()  # 传建一个日志对象，不创建，默认就是root
+logger.setLevel(logging.INFO)  # 这是log等级总开关
+
+# 第二步：创建一个handler，用于写入日志文件
+logfile = "./log.txt"
+fh = logging.FileHandler(logfile, mode="a", encoding="utf-8")
+fh.setLevel(logging.DEBUG)     # 输出到file中的log等级开关
+
+# 第三步：再创建一个handler，用于输出到控制台
+ch = logging.StreamHandler()
+ch.setLevel(logging.WARNING)   # 输出到console中的log等级开关
+
+# 第四步：定义handler的输出格式
+formatter = logging.Formatter("%(asctime)s - %(filename)s[line:%(lineno)d] - %(levelname)s: %(message)s")
+fh.setFormatter(formatter)
+ch.setFormatter(formatter)
+
+# 第五步：将logger添加到handler里面
+logger.addHandler(fh)
+logger.addHandler(ch)
+
+# 日志
+logging.debug("这是 logging debug message")
+logging.info("这是 logging info message")
+logging.warning("这是 logging warning message")
+logging.error("这是 logging error message")
+logging.critical("这是 logging critical message")
+
+# 这个是可以跨日志的
